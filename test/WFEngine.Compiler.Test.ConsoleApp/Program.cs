e04f493fd7372147b8a3689c88b70b2b16866edb
Ömer Faruk Şahin <faruk_thecno@hotmail.com>
2020-12-22 20:06:38 +0300

    Example Files Added.

--- test/WFEngine.Compiler.Test.ConsoleApp/Program.cs
+++ test/WFEngine.Compiler.Test.ConsoleApp/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Reflection;
 
 namespace WFEngine.Compiler.Test.ConsoleApp
 {
@@ -8,7 +6,7 @@ namespace WFEngine.Compiler.Test.ConsoleApp
     {
         [STAThread]
         static void Main(string[] args)
-        {          
+        {
             WFRunAtConsole runAtConsole = new WFRunAtConsole("workflows.json", "b40411ea-8eb5-40d6-8100-7c54d6fa9904", "eb6e253e-375b-44e6-92ff-a02c52f42973");
         }
     }
