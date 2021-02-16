0325cb5655a2015496e44448626245094c7a59de
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2021-02-16 21:04:10 +0300

    #7

--- test/WFEngine.Compiler.Test.ConsoleApp/Program.cs
+++ test/WFEngine.Compiler.Test.ConsoleApp/Program.cs
@@ -7,7 +7,7 @@ namespace WFEngine.Compiler.Test.ConsoleApp
         [STAThread]
         static void Main(string[] args)
         {
-            WFRunAtConsole runAtConsole = new WFRunAtConsole("workflows.json", "b40411ea-8eb5-40d6-8100-7c54d6fa9904", "eb6e253e-375b-44e6-92ff-a02c52f42973");
+            WFRunAtConsole runAtConsole = new WFRunAtConsole("workflows.json", "b40411ea-8eb5-40d6-8100-7c54d6fa9904", "a77be4ee-de5b-430d-9559-b662dc25cf12");
         }
     }
 }
