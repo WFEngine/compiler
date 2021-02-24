18ffab6874b4e63aa91decab72bbe7ead089adab
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2021-02-24 20:16:28 +0300

    .net 6.0 preview upgrade

--- test/WFEngine.Compiler.Test.ConsoleApp/Program.cs
+++ test/WFEngine.Compiler.Test.ConsoleApp/Program.cs
@@ -7,7 +7,7 @@ namespace WFEngine.Compiler.Test.ConsoleApp
         [STAThread]
         static void Main(string[] args)
         {
-            WFRunAtConsole runAtConsole = new WFRunAtConsole("workflows.json", "b40411ea-8eb5-40d6-8100-7c54d6fa9904", "a77be4ee-de5b-430d-9559-b662dc25cf12");
+            WFRunAtConsole runAtConsole = new WFRunAtConsole("workflows.json", "b40411ea-8eb5-40d6-8100-7c54d6fa9904", "09f11f7d-ffaf-409c-aa53-556aa4cc982e");
         }
     }
 }
